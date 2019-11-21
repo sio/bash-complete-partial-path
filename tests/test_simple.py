69afca2da91c0bce7a3522f5bdc4e6cd16ecfa49
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-11-21 15:04:33 +0300

    Test partial path completion

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,55 +2,55 @@
 Simple completion cases should be consistent with default behavior
 '''
 
-from pathlib import Path
 import pytest
 
 
-class TestSimpleCompletion:
+class TestPathCompletion:
     '''All tests in this class will run within the same instance of bash'''
 
     @pytest.mark.parametrize(
         'command,completion',
         (
+            # Default completer
             ('ls us', 'r'),
             ('cd us', 'r'),
             ('ls usr/sha', 're'),
             ('ls ', 'usr/'),
             ('cd usr/', 'share'),
-        )
-    )
-    def test_simple_one_result(self, bash, command, completion):
-        '''Complete unambiguos paths - default completer'''
-        assert bash.complete(command) == completion
 
-
-    @pytest.mark.parametrize(
-        'command,completions',
-        (
-            ('ls usr/s', ['usr/share', 'usr/somefile']),
-            ('ls usr/', ['share/', 'somefile']),
-            ('cd usr/share/a', ['usr/share/another', 'usr/share/anything', 'usr/share/applications']),
-            ('cd usr/share/', ['usr/share/another', 'usr/share/anything', 'usr/share/applications']),
-        )
-    )
-    def test_simple_many_results(self, bash, command, completions):
-        '''Complete ambiguous paths - default completer'''
-        completed = bash.complete(command)
-        assert completed
-        for variant in completions:
-            assert variant in completed
-
-
-    @pytest.mark.parametrize(
-        'command,completion',
-        (
+            # Partial path completer
             ('ls u/s/app', 'ls usr/share/applications'),
             ('cd u/s', 'cd usr/share'),
         )
     )
-    def test_bcpp_one_result(self, bash, command, completion):
-        '''Complete unambiguios paths - partial completer'''
+    def test_one_result(self, bash, command, completion):
+        '''Complete unambiguos paths'''
         assert bash.complete(command) == completion
+
+
+    @pytest.mark.parametrize(
+        'command,variants',
+        (
+            # Default completer
+            ('ls usr/s', ['usr/share', 'usr/somefile']),
+            ('ls usr/', ['share/', 'somefile']),
+            ('cd usr/share/a', ['usr/share/another', 'usr/share/anything', 'usr/share/applications']),
+            ('cd usr/share/', ['usr/share/another', 'usr/share/anything', 'usr/share/applications']),
+
+            # Partial path completer
+            ('ls u/s/a', ['usr/share/afile', 'usr/share/another', 'usr/share/anything', 'usr/share/applications']),
+            ('cd u/s/a', ['usr/share/another', 'usr/share/anything', 'usr/share/applications']),
+        )
+    )
+    def test_many_results(self, bash, command, variants):
+        '''Complete ambiguous paths'''
+        completed = bash.complete(command)
+        assert completed, 'empty completion'
+        for variant in variants:
+            assert variant in completed
+        for variant in variants:
+            completed = completed.replace(variant, '')
+        assert not completed.strip(), 'extra output in completion results'
 
 
 def test_debug(bash):
