8313b7b622f0af51993f2c3afad815b706f1ba24
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-11-21 12:23:40 +0300

    Use fixture to create filetree

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,5 +1,40 @@
-def test_simple():
-    assert 1 == 1
+'''
+Simple completion cases should be consistent with default behavior
+'''
 
-def test_bash(bash):
-    import pdb; pdb.set_trace()
+from pathlib import Path
+import pytest
+
+
+FILES = [
+    'usr/somefile',
+    'usr/share/afile',
+]
+DIRS = [
+    'usr/share/applications',
+    'usr/share/another',
+    'usr/share/anything',
+]
+
+
+@pytest.fixture
+def bash_filetree(bash):
+    '''Create filetree in bash session'''
+    root = Path(bash.tmpdir)
+    for dirname in DIRS:
+        (root / dirname).mkdir(parents=True)
+    for filename in FILES:
+        (root / filename).touch()
+    yield bash
+
+
+def test_simple_one_result(bash_filetree):
+    bash = bash_filetree
+    for command, completion in (
+            ('ls us', 'r'),
+            ('cd us', 'r'),
+            ('ls usr/sha', 're'),
+            ('ls ', 'usr/'),
+            ('cd usr/', 'share'),
+    ):
+        assert bash.complete(command) == completion
