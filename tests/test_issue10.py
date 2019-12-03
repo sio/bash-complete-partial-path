ffa2033c8892fd416587960fb00aaf76392511c1
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-12-03 18:15:10 +0300

    Tests for issue #10

---
+++ tests/test_issue10.py
@@ -0,0 +1,42 @@
+'''
+Tests for issue #10
+https://github.com/sio/bash-complete-partial-path/issues/10
+'''
+
+
+import shlex
+import pytest
+
+from tests.conftest import FILETREE
+
+
+class Test_Issue10:
+    '''Share bash instance between the following tests'''
+
+    DIR = 'usr/share'
+
+    @pytest.mark.parametrize('command', ['cd', 'ls'])
+    def test_single(self, bash, command):
+        '''Check that single tab adds trailing slash to directory name'''
+        completed = bash.complete('{} {}'.format(command, self.DIR))
+        assert completed == '/'
+
+    @pytest.mark.parametrize('command', ['cd', 'ls'])
+    def test_double(self, bash, command):
+        '''Check that double tab lists directory contents'''
+        completed = bash.complete('{} {}'.format(command, self.DIR), tabs=2)
+        variants = set(shlex.split(completed))
+        files = set(x for x in FILETREE['files'] if x.startswith(self.DIR))
+        dirs  = set(x for x in FILETREE['dirs'] if x.startswith(self.DIR))
+
+        if command == 'ls':
+            dirs = set(d + '/' for d in dirs)
+            dirs.add('/')
+            correct = set(x.replace(self.DIR + '/', '', 1) for x in files.union(dirs))
+            assert variants == correct
+        elif command == 'cd':
+            dirs.add('/')
+            assert variants == dirs
+        else:
+            assert False, 'unsupported command: {}'.format(command)
+
