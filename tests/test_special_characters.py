baa86782dade168b9fba9c62476451a4e3f1f22d
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-12-18 13:31:03 +0300

    Test completion for paths with special chars

---
+++ tests/test_special_characters.py
@@ -0,0 +1,33 @@
+'''
+Test enhanced completion for paths with special chars
+'''
+
+from pathlib import Path
+import pytest
+
+
+class TestSpecialCharacters:
+    '''All subtests will use the same bash session'''
+
+    @pytest.mark.parametrize(
+        'special,shortcut',
+        [
+            ('usr/share/a file with spaces', 'u/s/a'),
+            ('some/very/weird &file!', 's/v/w'),
+        ]
+    )
+    @pytest.mark.parametrize('quotes', ['', '"', "'"])
+    def test_special_chars(self, bash, special, shortcut, quotes):
+        '''Test special character handling'''
+        special_path = Path(bash.tmpdir) / special
+        special_path.parents[0].mkdir(parents=True, exist_ok=True)
+        special_path.touch()
+
+        command = 'ls {}{}'.format(quotes, shortcut)
+        output = bash.complete(command)
+
+        if len(output) == 1:
+            assert quotes + special in output.selected
+        if quotes:  # shlex fails when completion happens in open quotes
+            output = str(output)
+        assert special in output
