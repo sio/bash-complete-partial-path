8cc1a640070b413f2c912309010fba0028ed8744
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-12-18 16:19:12 +0300

    Test cooperation with scop/bash-completion

---
+++ tests/test_scop_completion.py
@@ -0,0 +1,24 @@
+import os
+import pytest
+
+from tests.conftest import STARTUP
+
+
+ENV_SCOP = 'BCPP_TEST_SCOP_COMPLETION'
+
+
+@pytest.mark.skipif(ENV_SCOP not in os.environ, reason='environment variable not set: %s' % ENV_SCOP)
+def test_scop_completion(bash):
+    '''Test compatibility with main bash-completion package'''
+    bash.execute('exec bash --norc --noprofile')
+    bash.execute('source "${}"'.format(ENV_SCOP))
+    for cmd in STARTUP:
+        bash.execute(cmd)
+
+    declare = bash.execute('declare -F')
+    assert '_filedir' in declare
+    assert '_bcpp_filedir' in declare
+
+    completion = bash.complete('ls u/s/a')
+    assert len(completion) > 1
+    assert 'usr/share/applications' in completion
