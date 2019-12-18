31b9b2b9d486147e1bd4a6a7f1d8ea1f879bf1d4
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-12-18 13:37:27 +0300

    Fix compatibility with Python 3.4

--- tests/test_special_characters.py
+++ tests/test_special_characters.py
@@ -20,7 +20,9 @@ class TestSpecialCharacters:
     def test_special_chars(self, bash, special, shortcut, quotes):
         '''Test special character handling'''
         special_path = Path(bash.tmpdir) / special
-        special_path.parents[0].mkdir(parents=True, exist_ok=True)
+        parent = special_path.parents[0]
+        if not parent.exists():
+            parent.mkdir(parents=True)  # exist_ok is not supported in Python 3.4
         special_path.touch()
 
         command = 'ls {}{}'.format(quotes, shortcut)
